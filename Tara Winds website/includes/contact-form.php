56784f0404aa1cd21a79e4400b246a540a5ff61b
Manjanka <masjaha88@gmail.com>
2017-09-11 19:19:35 +0300

    without photos, slow media page

---
+++ Tara Winds website/includes/contact-form.php
@@ -0,0 +1,16 @@
+<?php
+
+    $name = strip_tags(trim($_POST["name"]));
+            $name = str_replace(array("\r","\n"),array(" "," "),$name);
+    $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+    $phone = trim($_POST["phone"]);
+    $message = trim($_POST["message"]);
+
+    $emailto = 'andrea.strauss22@gmail.com ';
+
+    $msg = "Name: ".$name."   Email: ".$email."     Phone: ".$phone."      Message: ".$message;
+
+    $admin_email = "andrea.strauss22@gmail.com ";
+    mail($admin_email, "Tara Winds", $msg);
+
+?>
